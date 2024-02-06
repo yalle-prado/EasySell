9a79425e0f1985448207d40966ae54ad5a01b3c4
Yalle <yalle.prado@gmail.com>
2024-02-06 11:54:22 +0000

    Created Angular Interface

--- API/Program.cs
+++ API/Program.cs
@@ -12,13 +12,15 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt =>{
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+//app.UseHttpsRedirection();
+// app.UseAuthorization();
 
 app.MapControllers();
 
