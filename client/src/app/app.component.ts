aec678f5e32bf3fc8313617a1607de9a309f2e62
Yalle <yalle.prado@gmail.com>
2024-02-26 11:35:35 +0000

    Add Bootstrap components

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component , OnInit} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
